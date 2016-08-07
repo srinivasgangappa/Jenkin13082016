28b6b9dfc1dcdd0ce03fefeeecdb64754f831d10
srinivasgangappa <srinivasgangappa.87@gmail.com>
2016-08-07 19:01:31 +0530

    6

--- src/JenkinPackage/JenkinTest.java
+++ src/JenkinPackage/JenkinTest.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -14,7 +15,7 @@ public class JenkinTest
 	@Test
 	public void Jenkins() throws MalformedURLException
 	{
-	URL remoteaddress = new URL("http://10.10.4.179:4444/wd/hub");
+	/*URL remoteaddress = new URL("http://10.10.4.179:4444/wd/hub");
 	DesiredCapabilities c= new DesiredCapabilities();
 	c.setBrowserName("chrome");
 	c.setPlatform(Platform.WINDOWS);
@@ -23,6 +24,11 @@ public class JenkinTest
 	d.setBrowserName("firefox");
 	d.setPlatform(Platform.WINDOWS);
 	new RemoteWebDriver(remoteaddress,d);
-	driver.get("http://www.google.com/");
+	driver.get("http://www.google.com/");*/
+		
+	WebDriver driver = new FirefoxDriver();
+			driver.get("http://www.google.com/");
+		
+		
 	}
 }
